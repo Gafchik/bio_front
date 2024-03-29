f436f5d0cb4aecf42f06771e057149e1cee1ba97
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2024-03-29 23:21:49 +0200

    save commit

--- src/event-listener/event-listener.js
+++ src/event-listener/event-listener.js
@@ -1,7 +1,9 @@
 export const onScrollToLastItem = (selector,func) => {
     const element = document.querySelector(selector);
-    const rect = element.getBoundingClientRect();
-    if (rect.bottom <= (window.innerHeight)) {
-        func()
+    if(!!element){
+        const rect = element.getBoundingClientRect();
+        if (rect.bottom <= (window.innerHeight)) {
+            func()
+        }
     }
 }
